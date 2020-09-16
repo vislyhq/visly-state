bb9ae92d2185b4705bb419f0fcbfd4d0f3c8d804
Emil Sjölander <emil@visly.app>
2020-09-16 17:32:08 +0100

    Update footer text

--- website/src/components/Footer.js
+++ website/src/components/Footer.js
@@ -12,7 +12,7 @@ function Footer() {
     <footer className='footer'>
       <div className='footer-content'>
         <h1>Built by Visly</h1>
-        <p>Visly is amazing and we build cool shit. Read more about it in our blog. Visly is amazing and we build cool shit. Read more about it in our blog.</p>
+        <p>Visly is a design tool that allows you to design & build React components visually. We care deeply about quality - read more about what we do.</p>
       </div>
       <BlogButton/>
     </footer>
