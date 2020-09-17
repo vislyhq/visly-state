49269eccfae28c714232e65603995796a465e112
Sue Hitchins <69524939+sue-hitchins@users.noreply.github.com>
2020-09-17 12:05:40 +0100

    Add some logic and refactor a bit

--- examples/tic-tac-toe/client/README.md
+++ examples/tic-tac-toe/client/README.md
@@ -1,4 +1,4 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is an example project for visly-state.
 
 ## Available Scripts
 
--- examples/tic-tac-toe/client/src/App.tsx
+++ examples/tic-tac-toe/client/src/App.tsx
@@ -1,71 +1,136 @@
 import React from 'react'
-import { state, useValue, useMutation } from '@visly/state'
+import {state, useValue, useMutation} from '@visly/state'
 import nought from './assets/nought.svg'
 import cross from './assets/cross.svg'
 import './App.css'
 
-enum Player { 
+enum Player {
   X = 'X',
   O = 'O',
 }
 
-const emptyBoard = new Array(9).fill(null)
-const gameState = state({
+type Board = Array<null | Player>
+
+interface State {
+  board: Board
+  currentPlayer: Player
+  winner: null | Player
+}
+
+const emptyBoard: Board = new Array(9).fill(null)
+
+const initialState: State = {
   board: emptyBoard,
   currentPlayer: Player.O,
-})
+  winner: null,
+}
+
+const gameState = state<State>(initialState)
+
+const checkWinner = (board: Board): null | Player => {
+  // check horizontally
+  for (let i = 0; i < 3; i++) {
+    if (board[i * 3] === board[i * 3 + 1] && board[i * 3] === board[i * 3 + 2]) {
+      return board[i * 3]
+    }
+  }
+
+  // check vertically
+  for (let i = 0; i < 3; i++) {
+    if (board[i] === board[i + 3] && board[i] === board[i + 6]) {
+      return board[i]
+    }
+  }
+
+  // check diagonally
+  if (board[0] === board[4] && board[0] === board[8]) {
+    return board[0]
+  }
+  if (board[2] === board[4] && board[2] === board[6]) {
+    return board[2]
+  }
+
+  // no winner
+  return null;
+}
 
 const mutations = {
-  playSquare: (state: any, index: number, player: Player) => {
-      state.board[index] = player
+  playSquare: (state: State, index: number) => {
+    if (!state.board[index]) {
+      state.board[index] = state.currentPlayer
+      state.currentPlayer = state.currentPlayer === Player.X ? Player.O : Player.X
+      state.winner = checkWinner(state.board)
+    }
   },
-  alternatePlayer: (state: any) => {
-    state.currentPlayer = state.currentPlayer === Player.X ? Player.O : Player.X
-  }
 }
 
-function Square({ player, onClick}: { player: (Player | null), onClick: () => void}) {
-  return <div className={`square`} onClick={onClick}>
-    {player && <img src={Player.X ? cross : nought}/>}
-  </div>
+interface SquareProps {
+  player: null | Player
+  onClick?: () => void
 }
 
-function Board() {
+function Square({ player, onClick }: SquareProps) {
+  return (
+    <div className={`square`} onClick={onClick}>
+      {player && <img src={player === Player.X ? cross : nought}/>}
+    </div>
+  )
+}
 
+interface BoardProps {
+  disabled: boolean
+}
+
+function Board({disabled}: BoardProps) {
   const makeMove = useMutation(gameState, mutations.playSquare)
 
   const board = useValue(gameState, s => s.board)
-  const currentPlayer = Player.X
 
-  return <div className='board'>
-    <div className='row'>
-      <Square player={board[0]} onClick={() => makeMove(0, currentPlayer)}></Square>
-      <Square player={board[1]} onClick={() => makeMove(1, currentPlayer)}></Square>
-      <Square player={board[2]} onClick={() => makeMove(2, currentPlayer)}></Square>
+  return (
+    <div className='board'>
+      <div className='row'>
+        <Square player={board[0]} onClick={disabled ? undefined : () => makeMove(0)}/>
+        <Square player={board[1]} onClick={disabled ? undefined : () => makeMove(1)}/>
+        <Square player={board[2]} onClick={disabled ? undefined : () => makeMove(2)}/>
+      </div>
+      <div className='row'>
+        <Square player={board[3]} onClick={disabled ? undefined : () => makeMove(3)}/>
+        <Square player={board[4]} onClick={disabled ? undefined : () => makeMove(4)}/>
+        <Square player={board[5]} onClick={disabled ? undefined : () => makeMove(5)}/>
+      </div>
+      <div className='row'>
+        <Square player={board[6]} onClick={disabled ? undefined : () => makeMove(6)}/>
+        <Square player={board[7]} onClick={disabled ? undefined : () => makeMove(7)}/>
+        <Square player={board[8]} onClick={disabled ? undefined : () => makeMove(8)}/>
+      </div>
     </div>
-    <div className='row'>
-      <Square player={board[3]} onClick={() => makeMove(3, currentPlayer)}></Square>
-      <Square player={board[4]} onClick={() => makeMove(4, currentPlayer)}></Square>
-      <Square player={board[5]} onClick={() => makeMove(5, currentPlayer)}></Square>
-    </div>
-    <div className='row'>
-      <Square player={board[6]} onClick={() => makeMove(6, currentPlayer)}></Square>
-      <Square player={board[7]} onClick={() => makeMove(7, currentPlayer)}></Square>
-      <Square player={board[8]} onClick={() => makeMove(8, currentPlayer)}></Square>
-    </div>
-  </div>
+  )
 }
 
 function Game() {
-  return <div>
-    <Board/>
-  </div>
+  const currentPlayer = useValue(gameState, s => s.currentPlayer)
+  const winner = useValue(gameState, s => s.winner)
+
+  const reset = useMutation(gameState, s => s = initialState)
+
+  return (
+    <div>
+      {!winner && (
+        <div>{currentPlayer}'s move</div>
+      )}
+      {winner && (
+        <div>{winner} won!</div>
+      )}
+      <Board disabled={!!winner}/>
+      <div className='button' onClick={reset}>Reset</div>
+    </div>
+  )
 }
 
 
 function App() {
   return (
-    <Game></Game>
+    <Game/>
   )
 }
 
