58f68df7bf257308d40bb727d2925072817fdf49
Emil Sjölander <emil@visly.app>
2020-09-17 10:40:03 +0100

    Add backend start script

--- website/src/state.js
+++ website/src/state.js
@@ -1,6 +1,6 @@
 import { syncedState } from '@visly/state'
 
-export const state = syncedState({
+export const state = syncedState('bgstate', {
     bgSection1: 0,
     bgSection2: 1,
     bgSection3: 2,
