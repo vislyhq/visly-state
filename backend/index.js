21507be6af2c7e289ea66f0a0fdd8f928f903e51
Emil Sjölander <emil@visly.app>
2020-09-17 10:14:10 +0100

    Add basic state and backend code

---
+++ backend/index.js
@@ -0,0 +1,51 @@
+import "../website/src/state";
+import express from "express";
+import enableWs from "express-ws";
+import { setSyncAdapter, SyncPayloadType } from '@visly/state'
+
+const app = enableWs(express()).app;
+
+setSyncAdapter((applyPatches, setState) => {
+  const conections = new Set();
+  
+  app.ws("/", (ws) => {
+    conections.add(ws);
+
+    ws.on("message", (msg) => {
+      for (const connection of conections) {
+        if (connection !== ws && connection.readyState === WebSocket.OPEN) {
+          connection.send(msg);
+        }
+      }
+
+      const { type, key, data } = JSON.parse(msg);
+
+      switch (type) {
+        case SyncPayloadType.Patches:
+          applyPatches(key, data);
+          break;
+        case SyncPayloadType.FullSync:
+          setState(key, data);
+          break;
+      }
+    });
+
+    ws.on("close", () => {
+      conections.delete(ws);
+    });
+  });
+
+  return (key, patches) => {
+    conections.forEach((conn) => {
+      if (conn.readyState === WebSocket.OPEN) {
+        conn.send(
+          JSON.stringify({ type: SyncPayloadType.Patches, data: patches, key })
+        );
+      }
+    });
+  };
+});
+
+
+app.listen(process.env.PORT, () => console.log('server started'))
+
---
+++ website/src/state.js
@@ -0,0 +1,8 @@
+import { syncedState } from '@visly/state'
+
+export const state = syncedState({
+    bgSection1: 0,
+    bgSection2: 1,
+    bgSection3: 2,
+    bgSection4: 3,
+})
